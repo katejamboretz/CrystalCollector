ab21d1dfad315b18e0b12987d315fcb2aef4ed41
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-02-09 15:06:03 -0800

    link game.js file and pseudocode game strategy.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// set up array for random number generator between 19 and 120
+
+// set up array for gem number generator between 1 and 12
+
+// add data type to store data for each element for gem and random number id and display contents of random number generator
+
+// set score id data value to zero and display its contents
+
+// make on click events for each gem which add its value to the score value
+
+// make an if else function for when score = random number (add to win, reset random number, score and gem numbers)...
+//    score < random number (do nothing)
+//    score > random number (add to loss, reset random number, score and gem numbers)
